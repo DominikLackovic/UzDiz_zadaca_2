333a915928861151aec394d5a6edd134df678ebb
DominikLackovic <dominik.lackovic@outlook.com>
2017-11-13 22:05:04 +0100

    Prazan projekt

---
+++ src/dlackovi2_zadaca_2/Dlackovi2_zadaca_2.java
@@ -0,0 +1,18 @@
+package dlackovi2_zadaca_2;
+
+/**
+ *
+ * @author dlackovi2
+ */
+public class Dlackovi2_zadaca_2
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
