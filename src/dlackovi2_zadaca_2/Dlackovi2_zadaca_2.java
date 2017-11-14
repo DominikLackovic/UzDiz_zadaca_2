61df516dd04e8fdacf4c99d2da006d910ba88a2c
DominikLackovic <dominik.lackovic@outlook.com>
2017-11-14 01:02:03 +0100

    Validacija argumenata

--- src/dlackovi2_zadaca_2/Dlackovi2_zadaca_2.java
+++ src/dlackovi2_zadaca_2/Dlackovi2_zadaca_2.java
@@ -1,6 +1,14 @@
 package dlackovi2_zadaca_2;
 
 import dlackovi2_zadaca_2.rng.RandomNumberGenerator;
+import dlackovi2_zadaca_2.validation.ArgumentValidator;
+import dlackovi2_zadaca_2.validation.ValidArguments;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  *
@@ -12,8 +20,9 @@ public class Dlackovi2_zadaca_2
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws ParseException
     {
+        //ValidArguments validArguments = new ArgumentValidator(args).validate();
         
     }
 }
---
+++ src/dlackovi2_zadaca_2/validation/ValidArguments.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dlackovi2_zadaca_2.validation;
+
+/**
+ *
+ * @author dlackovi2
+ */
+public class ValidArguments
+{
+    private int seed;
+    private String placesFile;
+    private String sensorsFile;
+    private String actuatorsFile;
+    private String algorithm;
+    private int cycleDuration;
+    private int nCycle;
+    private String outputFile;
+    private int outputBuffer;
+
+    public int getSeed()
+    {
+        return seed;
+    }
+
+    public void setSeed(int seed)
+    {
+        this.seed = seed;
+    }
+
+    public String getPlacesFile()
+    {
+        return placesFile;
+    }
+
+    public void setPlacesFile(String placesFile)
+    {
+        this.placesFile = placesFile;
+    }
+
+    public String getSensorsFile()
+    {
+        return sensorsFile;
+    }
+
+    public void setSensorsFile(String sensorsFile)
+    {
+        this.sensorsFile = sensorsFile;
+    }
+
+    public String getActuatorsFile()
+    {
+        return actuatorsFile;
+    }
+
+    public void setActuatorsFile(String actuatorsFile)
+    {
+        this.actuatorsFile = actuatorsFile;
+    }
+
+    public String getAlgorithm()
+    {
+        return algorithm;
+    }
+
+    public void setAlgorithm(String algorithm)
+    {
+        this.algorithm = algorithm;
+    }
+
+    public int getCycleDuration()
+    {
+        return cycleDuration;
+    }
+
+    public void setCycleDuration(int cycleDuration)
+    {
+        this.cycleDuration = cycleDuration;
+    }
+
+    public int getnCycle()
+    {
+        return nCycle;
+    }
+
+    public void setnCycle(int nCycle)
+    {
+        this.nCycle = nCycle;
+    }
+
+    public String getOutputFile()
+    {
+        return outputFile;
+    }
+
+    public void setOutputFile(String outputFile)
+    {
+        this.outputFile = outputFile;
+    }
+
+    public int getOutputBuffer()
+    {
+        return outputBuffer;
+    }
+
+    public void setOutputBuffer(int outputBuffer)
+    {
+        this.outputBuffer = outputBuffer;
+    }
+
+    public ValidArguments(int seed, String placesFile, String sensorsFile, String actuatorsFile, String algorithm, int cycleDuration, int nCycle, String outputFile, int outputBuffer)
+    {
+        this.seed = seed;
+        this.placesFile = placesFile;
+        this.sensorsFile = sensorsFile;
+        this.actuatorsFile = actuatorsFile;
+        this.algorithm = algorithm;
+        this.cycleDuration = cycleDuration;
+        this.nCycle = nCycle;
+        this.outputFile = outputFile;
+        this.outputBuffer = outputBuffer;
+    }
+    
+    
+}
