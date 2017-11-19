a8cf79f3e67d50039dccb063e5aa9a558624f55e
DominikLackovic <dominik.lackovic@outlook.com>
2017-11-19 11:42:45 +0100

    Kostur visitora, interfejsi

---
+++ src/dlackovi2_zadaca_2/Visitor/Visitor.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dlackovi2_zadaca_2.Visitor;
+
+/**
+ *
+ * @author foobar
+ */
+public interface Visitor
+{
+    public void visit(/*Tip objekt*/);
+}
